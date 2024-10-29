4da075c31c22fdbc1e2c8ee26e8492b237fcdf41
Segtanof <zecta904@gmail.com>
2024-10-29 18:20:39 +0100

    finally worked out how to join the data set together now

--- read_occupation.py
+++ read_occupation.py
@@ -1,5 +1,26 @@
 import pandas as pd
 
-df = pd.read_csv('occupation.txt', sep='\t')
+#import occupation description
+df_occupation = pd.read_csv('occupation.txt', sep='\t')
+df_occupation = df_occupation.dropna(subset=["occupation_description"])
+df_occupation = df_occupation[["occupation_code", "occupation_name","occupation_description"]]
+df_occupation = df_occupation.rename(columns={"occupation_code": "OCC_CODE"})
+#print(df_occupation.head())
 
-print(df.head(10))
+#import wage stat
+df_wage = pd.read_excel("testocc.xlsx")
+df_wage = df_wage[["OCC_CODE", "A_MEAN", "O_GROUP"]]
+df_wage['OCC_CODE'] = df_wage['OCC_CODE'].str.replace('-', '').astype(int)
+df_wage = df_wage[df_wage['O_GROUP'] == "detailed"]
+#print(df_wage.head())
+
+#join both dfs together
+merged_df = pd.merge(df_occupation, df_wage, on='OCC_CODE', how='inner').drop(columns=["O_GROUP"])
+print(merged_df.head())
+
+#pass that to agent
+
+
+
+
+#get the result
