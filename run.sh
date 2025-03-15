b953ab368825d320dd31b8d39de516b38f5caa86
Segtanof <zecta904@gmail.com>
2025-03-16 00:57:14 +0100

    commit

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --ntasks=40
+#SBATCH --time=12:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --output=outputs/output.txt
+#SBATCH --error=outputs/error.txt
+#SBATCH --job-name=test
+#SBATCH --mem=128000
+
+python test_test.py
+# nvidia-smi
