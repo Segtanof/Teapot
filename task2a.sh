2b15c25f4c2c6565a25439a2e7e6f14189306767
Segtanof <zecta904@gmail.com>
2025-04-28 00:55:50 +0200

    commit

--- task2a.sh
+++ task2a.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=-50
+#SBATCH --job-name=1-200p
 #SBATCH --nodes=1              
 #SBATCH --ntasks=1             
 #SBATCH --cpus-per-task=2     
 #SBATCH --gres=gpu:1           
 #SBATCH --mem=4G              
-#SBATCH --time=01:00:00        
+#SBATCH --time=01:30:00        
 #SBATCH --output=outputs/output_%j.log
 #SBATCH --error=outputs/error_%j.log
 #SBATCH --mail-type=ALL
